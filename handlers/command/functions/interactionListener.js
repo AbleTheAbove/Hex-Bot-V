b766dcb4cba03ee160c42a87eca15854f7aa7963
Hex <hex.execute@gmail.com~>
2021-11-24 20:31:01 -0500

    Setup upcoming features in code

--- handlers/command/functions/interactionListener.js
+++ handlers/command/functions/interactionListener.js
@@ -1,21 +1,22 @@
 const cmdLog = require('./cmdLog.js')
 const optionFormatter = require('./optionFormatter.js')
 
-module.exports = Bot => {
-    Bot.Client.on('interactionCreate', interaction => {
-        if (!interaction.isCommand())
-            return
-        else {
-            let cmd = require(`../../../commands/${interaction.commandName}.js`)
+module.exports = (Bot) => {
+	Bot.Client.on('interactionCreate', (interaction) => {
+		if (!interaction.isCommand()) return
+		else {
+			let cmd = require(`../../../commands/${interaction.commandName}.js`)
 
-            if (interaction.member.permissions.has(cmd.permissions) || !cmd.permissions) {
-                let args = optionFormatter(interaction)
+			if (
+				interaction.member.permissions.has(cmd.permissions) ||
+				!cmd.permissions
+			) {
+				let args = optionFormatter(interaction)
 
-                cmdLog(Bot, interaction, cmd, args)
+				cmdLog(Bot, interaction, cmd, args)
 
-                return cmd.run(interaction, args)
-            } else
-                interaction.reply("You don't have permission to do that!")
-        }
-    })
+				return cmd.run(interaction, args)
+			} else interaction.reply("You don't have permission to do that!")
+		}
+	})
 }
